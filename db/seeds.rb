acd6391d1106eab8839e26f2ebcdb8fa7de25837
Irina <irenekochetkova@gmail.com>
2018-01-21 23:37:53 -0800

    added contact using the db/seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+contact = Contact.new(first_name: "Cameron", last_name: "Diaz", email: "camerondiaz@gmail.com", phone_number: "7384652")
+contact.save
